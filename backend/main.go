492e70961e7116ae408512d9a351d814bf5a2bff
Danuk-Udagama <danuk.udagama@stfc.ac.uk>
2025-03-28 16:28:04 +0000

    Added a simple api endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func main(){
-  fmt.Println("Hello world!")
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(requestBody *gin.Context) {
+		requestBody.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run("0.0.0.0:8080")
 }
